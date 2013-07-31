153d38811d5052412dddb4fd09edb00094fd993c
kyoungchin <kcseo.acm@gmail.com>
2013-07-31 23:09:21 +0900

    add method for getting frame and chance to knock down

--- src/bowling/BowlingGame.java
+++ src/bowling/BowlingGame.java
@@ -1,5 +1,22 @@
 package bowling;
 
 public class BowlingGame {
+	
+	private int frame = 1;
+	private int chanceToRoll  = 1;
+		
+	BowlingGame() {
+		
+	}
 
+	public int getFrame() {
+		return frame;
+	}
+
+	public int getChanceToRoll() {
+		return chanceToRoll;
+	}
+	
+	
+	
 }
--- test/bowling/BowlingGameTest.java
+++ test/bowling/BowlingGameTest.java
@@ -3,10 +3,14 @@ import junit.framework.TestCase;
 
 
 public class BowlingGameTest extends TestCase {
-
-
 	public void testCreate() throws Exception {
-		BowlingGame bowling = new BowlingGame();
+		BowlingGame game = new BowlingGame();
+		assertEquals(1, game.getFrame());
+		assertEquals(1, game.getChanceToRoll());
+		
+		
+		
+		
+		
 	}
-	
 }
