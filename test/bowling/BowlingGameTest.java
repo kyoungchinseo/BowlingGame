de181ae391ef7a65b6140499a0d0a6496e4b023c
kyoungchin <kcseo.acm@gmail.com>
2013-07-31 22:45:09 +0900

    create class (BowlingGame, BowlingGameTest)

---
+++ test/bowling/BowlingGameTest.java
@@ -0,0 +1,12 @@
+package bowling;
+import junit.framework.TestCase;
+
+
+public class BowlingGameTest extends TestCase {
+
+
+	public void testCreate() throws Exception {
+		BowlingGame bowling = new BowlingGame();
+	}
+	
+}
